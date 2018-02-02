f56f75dad4fe1fac1aaf8962fb809a2cf299ecca
Ramadhani99 <rsadiki99@gmail.com>
2018-02-02 23:01:23 +0300

    i have added css

---
+++ index.php
@@ -0,0 +1,18 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div id="seidebar">
+<div class="toogle_btn">
+	
+</div>
+	<u>
+	<li>HOME</li>
+	<li>About us</li>
+	<li>Contact us</li>
+	</u>
+</div>
+</body>
+</html>
