ade1d1485504e44791ef4ed3aad6be00599a961c
Ramadhani99 <rsadiki99@gmail.com>
2018-02-02 23:21:42 +0300

    I resolved the conlict

--- index.php
+++ index.php
@@ -4,15 +4,8 @@
 	<title></title>
 </head>
 <body>
-<div id="seidebar">
-<div class="toogle_btn">
-	
-</div>
-	<u>
-	<li>HOME</li>
-	<li>About us</li>
-	<li>Contact us</li>
-	</u>
-</div>
+
+
+
 </body>
 </html>
