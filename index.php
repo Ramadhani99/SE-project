971d5be9eb09ab880add516ce1b26d065dd0b25a
Ramadhani99 <rsadiki99@gmail.com>
2018-02-02 23:31:46 +0300

    make change

--- index.php
+++ index.php
@@ -4,8 +4,15 @@
 	<title></title>
 </head>
 <body>
-
-
-
+<div id="seidebar">
+<div class="toogle_btn">
+	
+</div>
+	<u>
+	<li>HOME</li>
+	<li>About us</li>
+	<li>Contact us</li>
+	</u>
+</div>
 </body>
 </html>
